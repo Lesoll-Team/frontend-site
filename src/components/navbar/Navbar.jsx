70b94c934df3554bb61b873c46fa16e649447246
AG-Jimmy <ahmedgamalzenhom@gmail.com>
2023-07-13 12:58:06 +0300

    add: menu user

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -10,40 +10,33 @@ import {
   MdOutlineRealEstateAgent,
   MdSearch,
   MdAddHome,
+  MdClear
 } from "react-icons/md";
 import { GiHamburgerMenu } from "react-icons/gi";
 
 export default function Navbar() {
   const [open, setOpen] = useState(true);
-  const [isAuth, setAuth] = useState(true);
+  const [isAuth, setAuth] = useState(false);
   const [language, setLanguage] = useState(true);
   const [openUserMenu, setOpenUserMenu] = useState(false);
 
+
   return (
-    <nav className="w-full   z-50 sticky top-0">{/**m-0 flex-row */}
-      <section className="flex bg-white drop-shadow-lg h-[90px] items-center">
-        {/**lg:justify-between justify-between items-center m-0*/}
-        {/*lg:justify-center justify-center*/}
-
-
-
-
+    <nav className="w-full   z-50 sticky top-0">
+      <section className="flex sm:justify-around  bg-white drop-shadow-lg h-[90px] items-center">
 
         {/*Logo */}
         <ul className=" flex  justify-center ">
           <li className="sm:w-[150px] w-[100px] flex  justify-center">
-            <Link className="" href={"/"}>
+            <Link className="" href={"/"}
+             onClick={()=>setOpenUserMenu(setOpen(true))}>
               <Image src={logoNavbar} width={150} height={150} />
             </Link>
           </li>
         </ul>
 
-
-
-
         {/*nav link web page */}
-        {/*w-5/12 h-auto  xl:space-x-8 lg:space-x-4 */}
-        <ul className="lg:flex   w-5/12 justify-center  hidden">
+        <ul className="md:flex   w-5/12 justify-center space-x-8 hidden">
 
           <li>
             <Link
@@ -76,46 +69,13 @@ export default function Navbar() {
           </li>
 
         </ul>
-        {/*nav link */}
-        {/* <ul className={`flex w-2/12 justify-center bg-blue-100 
-            ${isAuth ? "hidden" : "" }`}> */}
-        {/* </ul> */}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
            {/*button SignUp & user menu & user image*/}
-
-        <ul className="flex  grow justify-end items-center mr-6">            {/*p-1 justify-center*/}
-        
-        <li className={` ${isAuth ? "hidden" : "" }`}>{/*flex w-2/12 justify-center */}
-            <button>
+        <ul className="flex md:grow-0 justify-end grow bg-red-200 space-x-4 items-center">  
+        <li className={` ${isAuth ? "hidden" : "" }`}>
+            <button  onClick={()=>setOpenUserMenu(setOpen(true))}>
               <MdNotificationsNone className={` rounded-full 
-               text-darkGray bg-white sm:text-5xl text-3xl  hover:bg-darkGray hover:text-white  active:scale-95`}/> 
-               {/*sm:text-5xl text-2xl  border-b-[3px] */}
+               text-darkGray bg-white sm:text-5xl text-4xl  hover:bg-darkGray hover:text-white  active:scale-95`}/> 
             </button>
           </li>
 
@@ -123,9 +83,9 @@ export default function Navbar() {
           <li className={`${isAuth ? "" : "hidden"} `}>
             <button
               className=""
-              onClick={() => setAuth(!isAuth)}>{/**flex justify-center */}
+              onClick={() => setAuth(!isAuth)}>
               <Link
-                className=" p-1 font-medium xl:px-4  border-lightOrange border-[2px] sm:text-2xl text-[8px] bg-white 
+                className=" px-3 py-1 text-lg xl:px-4  border-lightOrange border-[2px] sm:text-2xl text-[8px] bg-white 
                 rounded-3xl duration-300 text-lightOrangeHover hover:bg-lightOrangeHover hover:text-white active:scale-95"
                 href="/signin">
                 {isAuth ? "Sign In" : "Log out"}
@@ -134,136 +94,124 @@ export default function Navbar() {
           </li>
 
           {/*user section*/}
-          <li className={`  ${isAuth ? "hidden" : ""} `}>
+          <li className={`  ${isAuth ? "hidden" : ""} bg-red-300  relative`}>
             <button onClick={() => setOpenUserMenu(!openUserMenu)}>
-              <img className="rounded-full sm:w-[50px] w-[28px] " src="https://fakeimg.pl/50/ff0000/" />
+              <img className="rounded-full sm:w-[50px] w-[40px] " src="https://fakeimg.pl/50/ff0000/" />
             </button>
+
+                        <ul className={`bg-gray-100 ${openUserMenu ? "" : "hidden"} rounded-md p-2 -left-24 w-36 absolute`}>
+                      
+                        <li>
+                          <Link
+                            className="   duration-300 text-darkGray hover:bg-gray-200 hover:text-black  active:scale-95"
+                            href={"/"}>
+                            
+                            {language ? "Profile" : "الصفحة الشخصية"}
+                          </Link>
+                        </li>
+                        <li>
+                          <button
+                            onClick={() => setLanguage(!language)}
+                            className="   duration-300 text-darkGray hover:bg-gray-200 hover:text-black  active:scale-95"
+                            >
+                            {language ? "عربى" : "English"}{" "}
+                          </button>
+                        </li>
+
+                        <li>
+                          <Link
+                            className="   duration-300 text-darkGray hover:bg-gray-200 hover:text-black  active:scale-95"
+                            href={"/signup"}>
+                            
+                            {language ? "SignUp" : "أشتراك"}{" "}
+                          </Link>
+                        </li>
+                        </ul>
+
           </li>
 
           {/*user menu*/}
-          <div className={`bg-gray-300 ${openUserMenu ? "" : "hidden"} `}>
-
-            <li>
-              <button
-                onClick={() => setLanguage(!language)}
-                className=" flex xl:p-2 p-1 xl:px-5 px-2  md:text-4xl text-3xl items-center  duration-300 text-darkGray hover:bg-gray-200 hover:text-black  active:scale-95"
-                >{/* */}
-                {language ? "عربى" : "English"}{" "}
-              </button>
-            </li>
-
-            <li>
-              <Link
-                className=" flex xl:p-2 p-1 xl:px-5 px-2  md:text-4xl text-3xl items-center  duration-300 text-darkGray hover:bg-gray-200 hover:text-black  active:scale-95"
-                href={"/signup"}>
-                {/* */}
-                {language ? "SignUp" : "أشتراك"}{" "}
-              </Link>
-            </li>
-          </div>
+       
 
         </ul>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
         {/*nav link web mobile */}
-        <ul className="flex mr-6  w-1/12 justify-center  lg:hidden">
+        <ul className="flex mr-6  w-1/12 bg-black justify-center  md:hidden">
           <li className="">
             <button
               className="flex justify-center "
               onClick={() => setOpen(!open)}
             >
               {open ? (
-                <GiHamburgerMenu className=" text-darkGreen sm:text-3xl	 text-xl	" />
+                <GiHamburgerMenu className=" text-darkGreen text-3xl		" />
               ) : (
-                <h1>X</h1>
+              <MdClear className=" text-darkGreen 	 text-4xl" />
               )}
             </button>
           </li>
         </ul>
       </section>
 
-      <div className="grid justify-items-stretch">
+
+{/*links in menu mobile button*/}
         <section
-          className={`w-full h-screen  justify-self-end bg-white lg:hidden ${
+          className={`  w-full h-screen  bg-white lg:hidden ${
             open
               ? "hidden"
               : `${openUserMenu ? `${setOpenUserMenu(false)}` : ``}`
-          } `}
+              } `}
         >
           <ul className="items-center">
             <li>
               <Link
-                className=" flex p-2 px-2 text-3xl  duration-300 text-darkGray hover:bg-gray-200 hover:text-black  active:scale-95"
+                className=" flex p-2 px-2 text-3xl justify-center duration-300 text-darkGray hover:bg-gray-200 hover:text-black  active:scale-95"
                 href={"/rent"}
+                onClick={()=>setOpenUserMenu(setOpen(true))}
               >
                 Rent
               </Link>
             </li>
             <li>
               <Link
-                className="flex p-2 px-2 text-3xl  duration-300 text-darkGray hover:bg-gray-200 hover:text-black  active:scale-95"
+                className="flex p-2 px-2 text-3xl justify-center duration-300 text-darkGray hover:bg-gray-200 hover:text-black  active:scale-95"
                 href={"/buy"}
+                onClick={()=>setOpenUserMenu(setOpen(true))}
               >
                 Buy
               </Link>
             </li>
             <li>
               <Link
-                className=" flex p-2 px-2 text-3xl duration-300 text-darkGray hover:bg-gray-200 hover:text-black  active:scale-95"
+                className=" flex p-2 px-2 text-3xl justify-center duration-300 text-darkGray hover:bg-gray-200 hover:text-black  active:scale-95"
                 href={"/sell"}
+                onClick={()=>setOpenUserMenu(setOpen(true))}
               >
                 Sell
               </Link>
             </li>
             <li>
               <Link
-                className=" flex p-2 px-2 text-3xl duration-300 text-darkGray hover:bg-gray-200 hover:text-black  active:scale-95"
+                className=" flex p-2 px-2 text-3xl justify-center duration-300 text-darkGray hover:bg-gray-200 hover:text-black  active:scale-95"
                 href={"/need"}
+                onClick={()=>setOpenUserMenu(setOpen(true))}
               >
                 Need
               </Link>
             </li>
             <li>
               <Link
-                className=" flex p-2 px-2 text-3xl duration-300 text-darkGray hover:bg-gray-200 hover:text-black  active:scale-95"
+                className=" flex p-2 px-2 text-3xl justify-center duration-300 text-darkGray hover:bg-gray-200 hover:text-black  active:scale-95"
                 href={"/about"}
+                onClick={()=>setOpenUserMenu(setOpen(true))}
               >
                 About us
               </Link>
             </li>
             <li>
               <Link
-                className="flex p-2 px-2 text-3xl  duration-300 text-darkGray hover:bg-gray-200 hover:text-black  active:scale-95"
+                className="flex p-2 px-2 text-3xl justify-center duration-300 text-darkGray hover:bg-gray-200 hover:text-black  active:scale-95"
                 href={"/contact"}
+                onClick={()=>setOpenUserMenu(setOpen(true))}
               >
                 Contact
               </Link>
@@ -271,16 +219,13 @@ export default function Navbar() {
             <li className={`${isAuth?"":"hidden"}`}>
               <button
                 onClick={() => setLanguage(!language)}
-                className=" w-full flex p-2 px-2 text-3xl duration-300 text-darkGray hover:bg-gray-200 hover:text-black  active:scale-95"
+                className=" w-full flex p-2 px-2 text-3xl justify-center duration-300 text-darkGray hover:bg-gray-200 hover:text-black  active:scale-95"
               > {/* */}
                 {language ? "  عربى  " : "English"}
               </button>
             </li>
           </ul>
         </section>
-      </div>
-
-      {/**---code --- hidden */}
     </nav>
   );
 }
