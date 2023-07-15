9fcc8bfb202184c3d6d4261b639415132226f704
abdelrahmanmostafa0 <abdelrahmanmostafa.developer@gmail.com>
2023-07-15 07:17:01 +0300

    footer responsivness and social links

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,20 +4,77 @@ import Link from "next/link";
 const Footer = () => {
   return (
     <footer>
-      <div className="container mx-auto lg:grid grid-cols-5 gap-10 py-10">
-        <div className="col-span-2">
+      <div className="container mx-auto md:grid lg:grid-cols-5 grid-cols-3 gap-10 py-10 space-y-4">
+        {/* Discription */}
+        <div className="col-span-3 lg:col-span-2 space-y-3 relative">
+          {/* logo */}
           <Image
-            width={250}
-            height={250}
-            className="object-fit -mt-5"
+            width={100}
+            height={100}
+            // className=" "
+            className="object-fit -mt-5 lg:w-[250px]  w-[150px]"
             src="/logo-x.png"
           />
-
           <p className="w-3/4">
             Lesoll is an Egyptian RealEstate marketplace. lesoll offers
             customers a digital experiance for buying sending and renting
             properties with end to end service
           </p>
+          {/* social links */}
+          <div className="flex md:gap-3 gap-1 absolute top-0 right-0 lg:static">
+            <div className=" rounded-full bg-darkGray flex justify-center items-center">
+              <a
+                target="_blank"
+                href="https://www.facebook.com/LesollRealestate"
+                className="text-white"
+              >
+                <Image
+                  className="w-[25px] md:w-[40px]  object-fit"
+                  src={"/icons/facebook.svg"}
+                  width={40}
+                  height={40}
+                ></Image>
+              </a>
+            </div>
+            <div className=" rounded-full bg-darkGray flex justify-center items-center">
+              <a
+                target="_blank"
+                href="https://www.instagram.com/lesollrealestate/"
+                className="text-white"
+              >
+                <Image
+                  src={"/icons/instgaram.svg"}
+                  width={40}
+                  height={40}
+                  className="w-[25px] md:w-[40px]  object-fit"
+                ></Image>
+              </a>
+            </div>
+            <div className=" rounded-full bg-darkGray flex justify-center items-center">
+              <a target="_blank" href="#" className="text-white">
+                <Image
+                  className="w-[25px] md:w-[40px] object-fit"
+                  src={"/icons/telegram.svg"}
+                  width={40}
+                  height={40}
+                ></Image>
+              </a>
+            </div>
+            <div className=" rounded-full bg-darkGray flex justify-center items-center">
+              <a
+                target="_blank"
+                href="https://twitter.com/LesollRealstate"
+                className="text-white"
+              >
+                <Image
+                  className="w-[25px] md:w-[40px]  object-fit"
+                  src={"/icons/twitter.svg"}
+                  width={40}
+                  height={40}
+                ></Image>
+              </a>
+            </div>
+          </div>
         </div>
         <div className="space-y-5 flex flex-col">
           <h3 className="text-1xl font-bold">Useful Links</h3>
@@ -51,8 +108,9 @@ const Footer = () => {
           <a href="mailto:info@lesoll.com">Email:info@lesoll.com</a>
         </div>
       </div>
+      {/* policy & copy Rights */}
       <div className="bg-darkGreen py-3  w-full text-white">
-        <div className="container mx-auto flex justify-between ">
+        <div className="container mx-auto sm:flex-row flex-col-reverse flex justify-between items-center ">
           <p>
             © {new Date().getFullYear()},{` `}
             <a href="https://lesoll.com">Lesoll.com</a>
