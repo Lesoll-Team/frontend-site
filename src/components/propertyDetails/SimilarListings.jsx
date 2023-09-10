13ca1203880970cba01de63dd5b5565deed37aa2
abdelrahmanmostafa0 <abdelrahmanmostafa.developer@gmail.com>
2023-09-10 11:51:44 +0300

    fix details

--- src/components/propertyDetails/SimilarListings.jsx
+++ src/components/propertyDetails/SimilarListings.jsx
@@ -4,11 +4,11 @@ import { ar } from "../../language/ar/common";
 import { en } from "../../language/en/common";
 function SimilarListings({ recommendationsProperty }) {
   return (
-    <div className=" overflow-x-hidden p-10 px-14 border-2 mb-8 border-gray-200 rounded-3xl">
+    <div className=" overflow-x-hidden p-2 md:p-10 md:px-14 border-2 mb-5 border-gray-200 rounded-3xl">
       {/* <div><h1>Similar Listings</h1></div> */}
       <div className="font-bold mb-6 ml-5">
         {" "}
-        <b className="sm:text-3xl text-lg"> Similar Listings</b>
+        <b className="sm:text-3xl text-lg p-3"> Similar Listings</b>
       </div>
       <div className="lg:flex">
         {recommendationsProperty.map((recommendations) => (
