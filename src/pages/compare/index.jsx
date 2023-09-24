79f2e4ef7243f33e40464d659265125e3f3873dd
AbdelrahmanMostafa0 <abdelrahmanmostafa.developer@gmail.com>
2023-09-24 22:46:53 +0300

    compare error fix for build

--- src/pages/compare/index.jsx
+++ src/pages/compare/index.jsx
@@ -1,17 +1,17 @@
 // import LoadingPage from '@/Shared/lodaingPage'
-import CompareCards from '@/components/compare/CompareCards'
+import CompareCards from "@/components/compare/CompareCards";
 // import CompareDetails from '@/components/compare/CompareDetails'
-import CompareHeader from '@/components/compare/CompareHeader'
-import React from 'react'
+import CompareHeader from "@/components/compare/CompareHeader";
+import React from "react";
 
 function index() {
   return (
-    <div className='min-h-[90dvh] '>
-    <CompareHeader />
-    <CompareCards />
-    {/* <CompareDetails /> */}
+    <div className="min-h-[90dvh] ">
+      {/* <CompareHeader />
+    <CompareCards /> */}
+      {/* <CompareDetails /> */}
     </div>
-  )
+  );
 }
 
-export default index
+export default index;
