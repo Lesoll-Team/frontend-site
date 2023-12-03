fa5f5df4e74955edbae142c85b959be3e869717f
AG-Jimmy <ahmedgamalzenhom@gmail.com>
2023-12-03 16:33:08 +0200

    edit blog

--- src/components/blogs/blogCard.jsx
+++ src/components/blogs/blogCard.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import {  Image } from "@nextui-org/react";
 import Link from "next/link";
 import { useSelector } from "react-redux";
+import { BsBoxArrowInDownRight } from "react-icons/bs";
 
 function BlogCard({ blogData }) {
   const language = useSelector((state) => state.GlobalState.languageIs);
@@ -13,8 +14,7 @@ function BlogCard({ blogData }) {
           <div className=" w-[350px]   mx-auto">
             <Link
               title={language ? item.title.ar : item.title.en}
-              href={`/blog/${language ? item.slug.ar : item.slug.en}`}
-            >
+              href={`/blog/${language ? item.slug.ar : item.slug.en}`}>
               <Image
                 width="100%"
                 height="100%"
@@ -25,18 +25,33 @@ function BlogCard({ blogData }) {
               />
             </Link>
           </div>
-
-          <div className=" m-2">
-            <div className="">
-              <h2 className="text-lightGreen sm:text-lg line-clamp-1 flex-wrap text-sm text-md md:text-xl lg:text-2xl ">
-                {language ? item.title.ar : item.title.en}
-                {language ? item.title.ar : item.title.en}
-              </h2>
+          <div className="m-2">
+            <div className="my-2">
+              <Link
+                title={language ? item.title.ar : item.title.en}
+                href={`/blog/${language ? item.slug.ar : item.slug.en}`}
+              >
+                <h2 className="text-lightGreen sm:text-lg line-clamp-1 flex-wrap text-sm text-md md:text-xl lg:text-2xl ">
+                  {language ? item.title.ar : item.title.en}
+                  {language ? item.title.ar : item.title.en}
+                </h2>
+              </Link>
             </div>
-            <div className="">
+            <div className="my-2 mb-3">
               <p className="text-default-500 sm:text-lg line-clamp-2 flex-wrap text-sm  md:text-medium lg:text-lg ">
                 {language ? item.metaDescription.ar : item.metaDescription.en}
               </p>
+            </div>
+            <div className=" flex justify-center ">
+              <Link
+                title={language ? item.title.ar : item.title.en}
+                href={`/blog/${language ? item.slug.ar : item.slug.en}`}
+                className="text-blue-600 underline  flex items-center">
+                <div className="mx-2">
+                  <BsBoxArrowInDownRight />
+                </div>
+                {language ? "قراءة المزيد" : "read more"}
+              </Link>
             </div>
           </div>
         </div>
--- src/components/linksInHome/LinksSearch.jsx
+++ src/components/linksInHome/LinksSearch.jsx
@@ -8,7 +8,7 @@ function LinksSearch({ PopularSearches, MostArea, MostGovernorate }) {
   return (
     <section className="overflow-hidden xl:justify-center justify-normal  flex flex-wrap gap-y-5 gap-x-10  py-5 pb-14">
       <div className="w-[350px] ">
-        <h4 className="text-xl md:text-xl lg:text-2xl">
+        <h4 className="text-xl md:text-xl font-semibold lg:text-2xl">
           الكلمات الأكثر استخداما
         </h4>
         {PopularSearches.map((links, index) => (
@@ -21,7 +21,7 @@ function LinksSearch({ PopularSearches, MostArea, MostGovernorate }) {
               }
               className="w-max line-clamp-1"
             >
-              <h6 className="text-lightGreen line-clamp-1 text-base md:text-md lg:text-lg">
+              <h6 className="text-default-900 line-clamp-1 text-base md:text-md lg:text-lg">
                 {language ? links.name.title.ar : links.name.title.en}
               </h6>
             </Link>
@@ -30,7 +30,9 @@ function LinksSearch({ PopularSearches, MostArea, MostGovernorate }) {
       </div>
 
       <div className="w-[350px]">
-        <h4 className="text-xl md:text-xl lg:text-2xl">الأماكن الأكثر بحثاً</h4>
+        <h4 className="text-xl md:text-xl font-semibold lg:text-2xl">
+          الأماكن الأكثر بحثاً
+        </h4>
         {MostArea.map((links, index) => (
           <div key={index} className="my-2 w-auto flex">
             <Link
@@ -41,7 +43,7 @@ function LinksSearch({ PopularSearches, MostArea, MostGovernorate }) {
               }
               className="w-max line-clamp-1"
             >
-              <h6 className="text-lightGreen text-base md:text-md line-clamp-1 lg:text-lg">
+              <h6 className="text-default-900 text-base md:text-md line-clamp-1 lg:text-lg">
                 {language ? links.name.title.ar : links.name.title.en}
               </h6>
             </Link>
@@ -50,7 +52,9 @@ function LinksSearch({ PopularSearches, MostArea, MostGovernorate }) {
       </div>
 
       <div className="w-[350px]">
-        <h4 className="text-xl md:text-xl lg:text-2xl">المناطق الأكثر شعبية</h4>
+        <h4 className="text-xl md:text-xl lg:text-2xl font-semibold">
+          المناطق الأكثر شعبية
+        </h4>
         {MostGovernorate.map((links, index) => (
           <div key={index} className="my-2 w-auto flex">
             <Link
@@ -61,7 +65,7 @@ function LinksSearch({ PopularSearches, MostArea, MostGovernorate }) {
               }
               className="w-max line-clamp-1"
             >
-              <h6 className="text-lightGreen line-clamp-1 text-base md:text-md lg:text-lg">
+              <h6 className="text-default-900 line-clamp-1 text-base md:text-md lg:text-lg">
                 {language ? links.name.title.ar : links.name.title.en}
               </h6>
             </Link>
