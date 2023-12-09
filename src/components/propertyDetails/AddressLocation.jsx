bebde2d279863b1fa4d7c07d231be36b06fe7b22
AbdelrahmanMostafa0 <abdelrahmanmostafa.developer@gmail.com>
2023-12-09 22:52:57 +0200

    put the address apove the map

--- src/components/propertyDetails/AddressLocation.jsx
+++ src/components/propertyDetails/AddressLocation.jsx
@@ -1,11 +1,10 @@
 import { memo } from "react";
 import { FaMapLocationDot } from "react-icons/fa6";
-
 import { useSelector } from "react-redux";
 
 function AddressLocation({ singleAddressLocation }) {
   const language = useSelector((state) => state.GlobalState.languageIs);
-
+  console.log(singleAddressLocation);
   const openDirectionsInGoogleMaps = () => {
     const url = `https://www.google.com/maps/dir/?api=1&destination=${singleAddressLocation?.address.latitude},${singleAddressLocation?.address.longitude}`;
     window.open(url, "_blank");
@@ -15,20 +14,25 @@ function AddressLocation({ singleAddressLocation }) {
       <p className="sm:text-4xl text-lg  font-bold text-lightOrange  mb-2">
         {language ? " موقع العقار" : "Property location"}
       </p>
-      <div
-        className=" relative overflow-hidden w-full rounded-lg "
-        onClick={openDirectionsInGoogleMaps}
-      >
-        <button className="flex items-center gap-1 z-10 absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] px-7 rounded-md py-2 bg-white hover:scale-110 duration-200 text-darkGreen font-medium">
-          {language ? "الموقع على الخريطة" : "Location on the map"}
-          <FaMapLocationDot className="text-darkGreen" />
-        </button>
-        <img
-          src="/map.jpg"
-          alt="map"
-          className="w-full cursor-pointer hover:scale-105 duration-200 rounded-lg  drop-shadow-xl max-h-[300px]  object-cover"
-        />
-        {/* <div className=" lg:w-[100%]  h-[300px]">
+      <div className="space-y-2">
+        <p className="text-sm text-darkGreen">
+          {singleAddressLocation?.address.governrate},{" "}
+          {singleAddressLocation?.address.region}
+        </p>
+        <div
+          className=" relative overflow-hidden w-full rounded-lg "
+          onClick={openDirectionsInGoogleMaps}
+        >
+          <button className="flex items-center gap-1 z-10 absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] px-7 rounded-md py-2 bg-white hover:scale-110 duration-200 text-darkGreen font-medium">
+            {language ? "الموقع على الخريطة" : "Location on the map"}
+            <FaMapLocationDot className="text-darkGreen" />
+          </button>
+          <img
+            src="/map.jpg"
+            alt="map"
+            className="w-full cursor-pointer hover:scale-105 duration-200 rounded-lg  drop-shadow-xl max-h-[300px]  object-cover"
+          />
+          {/* <div className=" lg:w-[100%]  h-[300px]">
         <ShowMap
           center={{
             lat: parseFloat(singleAddressLocation.address.latitude),
@@ -36,6 +40,7 @@ function AddressLocation({ singleAddressLocation }) {
           }}
         />
       </div> */}
+        </div>
       </div>
     </div>
   );
