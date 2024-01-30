8638fb887a2652f07701bdd244fe50ac1e1c17f6
Abdelrahman Mostafa <abdelrahmanmostafa.developer@gmail.com>
2024-01-30 23:51:24 +0200

    fix: import the compaonent reset pass into the page correctly

--- src/pages/resetpassword/[token].jsx
+++ src/pages/resetpassword/[token].jsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import ResetPassword from "../../components/resetPassword/ResetPassword"
+import React from "react";
+import ResetPassword from "../../components/auth/resetPassword/ResetPassword";
 function ResetPasswordPage() {
   return (
-    <div className='container mx-auto  min-h-[95vh] flex items-center justify-center '>
-    <ResetPassword />
+    <div className="container mx-auto  min-h-[95vh] flex items-center justify-center ">
+      <ResetPassword />
     </div>
-  )
+  );
 }
 
-export default ResetPasswordPage
+export default ResetPasswordPage;
