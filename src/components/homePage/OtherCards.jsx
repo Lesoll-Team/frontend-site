a867f14662557459e5758716de95a396415ac34d
AG-Jimmy <ahmedgamalzenhom@gmail.com>
2024-03-04 17:44:25 +0200

    hotfix: edit other cards href

--- src/components/homePage/OtherCards.jsx
+++ src/components/homePage/OtherCards.jsx
@@ -9,7 +9,7 @@ const OtherCards = () => {
   return (
     <Fragment>
       <Link
-        href="/sell"
+        href="/add-property"
         className="flex lg:h-[205px] h-[120px] p-5  gap-x-3 cursor-pointer justify-around items-center md:w-[49%] w-full bg-gradient-to-b border border-gray-100 from-[#e2f5ff] to-[#8ed9ff00] rounded-lg shadow"
       >
         <label className="min-w-[70%] max-w-[71%]   cursor-pointer">
@@ -33,7 +33,7 @@ const OtherCards = () => {
       </Link>
 
       <Link
-        href="/"
+        href="/add-need"
         className="flex p-5 lg:h-[205px] h-[120px]  cursor-pointer justify-around items-center md:w-[49%] w-full  bg-gradient-to-b border border-gray-100 from-[#e2f5ff] to-[#8ed9ff00] rounded-lg shadow"
       >
         <label className="min-w-[70%] cursor-pointer">
