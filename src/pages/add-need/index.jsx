b8d66450be3ec94b59279d2f6ebae99de9418ea0
AbdelrahmanMostafa0 <abdelrahmanmostafa.developer@gmail.com>
2024-03-08 11:57:59 +0200

    add a title and description for the add need

--- src/pages/add-need/index.jsx
+++ src/pages/add-need/index.jsx
@@ -1,11 +1,22 @@
 import AddNeed from "@/components/needs/addNeed/AddNeed";
 import Head from "next/head";
+import { useSelector } from "react-redux";
 
 export default function need() {
+  const language = useSelector((state) => state.GlobalState.languageIs);
+
   return (
     <div className="min-h-[80dvh]  ">
       <Head>
-        <title>Lesoll Need</title>
+        <title>{language ? "أضف طلبك | ليسول" : ""}</title>
+        <meta
+          name="description"
+          content={
+            language
+              ? "اضف طلبك للعقار الذى تبحث عنه كى نساعدك على ايجاده"
+              : "Add your need to the property you are looking for so we can help you find it"
+          }
+        />
       </Head>
       <AddNeed />
     </div>
