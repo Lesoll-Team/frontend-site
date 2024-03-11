3b8150ce926ca2c0e3a5e1ba5ef6c2576417f353
abdelrahmanmistafa0 <abdelrahmanmostafa.developer@gmail.com>
2024-03-11 10:53:16 +0200

    project card

---
+++ src/components/dashboard/router/all-projects/components/ProjectCard.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import DeleteProject from "./modals/DeleteModal";
+import Image from "next/image";
+import Link from "next/link";
+import { useSelector } from "react-redux";
+
+const ProjectCard = ({ data }) => {
+  const language = useSelector((state) => state.GlobalState.languageIs);
+  return (
+    <div className="flex md:flex-row flex-col border drop-shadow p-2 bg-white">
+      <div className="flex items-start gap-3 md:flex-row flex-col w-full ">
+        <Link
+          href={"/"}
+          className="object-cover w-full md:max-w-[150px] max-h-[150px] border rounded-md"
+        >
+          <Image
+            src={data?.thumbnail}
+            width={300}
+            height={300}
+            className="object-cover w-full md:max-w-[150px] max-h-[150px] border rounded-md"
+          />
+        </Link>
+        <div className="flex flex-col justify-between w-full h-full gap-2">
+          <div className="h-full space-y-1">
+            <h3 className="text-xl font-bold">
+              {language ? data?.titleAr : data?.titleEn}
+            </h3>
+            <p className="line-clamp-2 text-outLine break-all">
+              {data.description}
+            </p>
+          </div>
+          <div className="flex w-full">
+            {/* <Link href={} */}
+            <DeleteProject id={data._id} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectCard;
