eaad5225a0c46162bf75e9dc5ea9aba812762b73
AG-Jimmy <ahmedgamalzenhom@gmail.com>
2024-03-13 07:58:30 +0200

    added : canonical url in categories

--- src/components/category/shared/HeadMetaTags.jsx
+++ src/components/category/shared/HeadMetaTags.jsx
@@ -1,10 +1,17 @@
 import Head from "next/head";
+import { useRouter } from "next/router";
 import React from "react";
 import { useSelector } from "react-redux";
 
 const HeadMetaTags = ({ result }) => {
+  const siteUrl=process.env.NEXT_PUBLIC_API_LOCAL_DOMAIN
   const language = useSelector((state) => state.GlobalState.languageIs);
-
+    const router = useRouter();
+  const cleanPath = router.asPath.split('#')[0];
+  const canonicalUrl = `${siteUrl}` + (router.asPath === '/' ? '' : cleanPath);
+console.log("canonicalUrl router::::",canonicalUrl);
+// console.log(" router::::",router);
+// console.log(" router asPath ::::",router.asPath);
   return (
     <Head>
       <title>
@@ -14,6 +21,8 @@ const HeadMetaTags = ({ result }) => {
         name="description"
         content={`${result?.supTitleCategory?.ar} لدينا العديد من العقارات في مصر، شقق، اراضي، محلات تجارية.  اتصل بنا واكتشف مجموعة متنوعة من الخيارات المتاحة  اراضي، محلات تجارية, شقة `}
       />
+          <link rel="canonical" href={canonicalUrl} />
+
     </Head>
   );
 };
