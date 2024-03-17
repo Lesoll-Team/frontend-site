4ab3608c54ef5affb6a40fd17b5b036ec575842f
AG-Jimmy <ahmedgamalzenhom@gmail.com>
2024-03-17 12:23:38 +0200

    edit: meta tag to home page

--- src/components/homePage/HomeMetaTag.jsx
+++ src/components/homePage/HomeMetaTag.jsx
@@ -1,7 +1,10 @@
 import Head from 'next/head';
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 const HomeMetaTag = () => {
+    const language = useSelector((state) => state.GlobalState.languageIs);
+
     const structuredData = {
         "@context": "https://schema.org",
         "@graph": [
@@ -106,9 +109,26 @@ const HomeMetaTag = () => {
     };
     return (
         <Head>
+            <title>
+                {language
+                    ? "بيع - شراء - إيجار العقارات فى مصر | ليسول"
+                    : "Lesoll Real Estate Hup Eg | Buy - Sell - Rent Properties"}
+            </title>
             <script type="application/ld+json">
                 {JSON.stringify(structuredData)}
             </script>
+            <meta
+                name="description"
+                content="موقع عقارات مصري متكامل. يساعدك في البحث عن العقار المناسب للبيع أو الإيجار شقة او فيلا او بيت وغيره من العقارات في القاهرة، الجيزة، الاسكندرية وجميع انحاء مصر"
+            />
+            <meta
+                property="og:description"
+                content="موقع عقارات مصري متكامل. يساعدك في البحث عن العقار المناسب للبيع أو الإيجار شقة او فيلا او بيت وغيره من العقارات في القاهرة، الجيزة، الاسكندرية وجميع انحاء مصر"
+            />
+            <meta
+                name="keywords"
+                content="Real estate, Property listings, Homes for sale, Apartments for rent, Commercial properties, Realtor, Real estate agent, House hunting, Real estate market, Property management, Real estate investment, Rental properties, Property search, Buy a house, Sell a house, Foreclosures, Mortgage rates, Property valuation, Open house, Home inspection, عقارات, قائمة العقارات, منازل للبيع, شقق للإيجار, عقارات تجارية, وكيل عقاري, سوق العقارات, إدارة العقارات, استثمار العقارات, عقارات للإيجار, البحث عن عقار, شراء منزل, بيع منزل, العقارات المستصرفة, أسعار الرهن العقاري, تقييم العقار, منازل مفتوحة للزيارة, فحص المنزل"
+            />
         </Head>
     );
 }
