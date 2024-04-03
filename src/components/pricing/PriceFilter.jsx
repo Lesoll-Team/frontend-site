c964cea89b6138bc31ba38acf2e49232c2bb4004
AG-Jimmy <ahmedgamalzenhom@gmail.com>
2024-04-03 17:09:25 +0300

    Feature: create yime filter price

--- src/components/pricing/PriceFilter.jsx
+++ src/components/pricing/PriceFilter.jsx
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
 
 const PriceFilter = () => {
+  const language = useSelector((state) => state.GlobalState.languageIs);
+  const [timeFilter, setTimeFilter] = useState("");
   return (
-    <div>
-      <h1>Filter</h1>
+    <div className="w-full flex justify-center ">
+      <div className="flex gap-x-2">
+        <button>{language ? "سنوية" : "Yearly"}</button>
+        <button>{language ? "شهرية" : "Monthly"}</button>
+        <button>{language ? "يومية" : "Daily"}</button>
+        <button>{language ? "تحت إدارة ليسول" : "Managed by Lesoll"}</button>
+      </div>
     </div>
   );
 };
