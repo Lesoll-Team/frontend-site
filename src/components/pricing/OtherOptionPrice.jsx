79bbe639896897a86a6a7db98bedc18112be9039
AG-Jimmy <ahmedgamalzenhom@gmail.com>
2024-04-04 13:16:47 +0300

    Feature: filter &option customize

--- src/components/pricing/OtherOptionPrice.jsx
+++ src/components/pricing/OtherOptionPrice.jsx
@@ -1,9 +1,37 @@
+import Link from "next/link";
 import React from "react";
+import { useSelector } from "react-redux";
 
 const OtherOptionPrice = () => {
+  const language = useSelector((state) => state.GlobalState.languageIs);
+
   return (
-    <div>
-      <h1>Other Option Price</h1>
+    <div
+      className="md:container md:mx-auto sm:mx-[20px] mx-[10px] 
+       w-full"
+    >
+      <div className="bg-[#F2F8F9] md:px-0 px-[2vh]  sm:h-[18vh] w-full flex sm:flex-nowrap flex-wrap  items-center md:justify-around sm:justify-between justify-center gap-x-4">
+        <div className="sm:py-[4vh] py-[0.9vh] flex flex-col sm:items-start items-center">
+          <h2 className="text-[#666666]">
+            {language
+              ? "تريد باقة مخصصة لك ؟"
+              : "You want a customized package?"}
+          </h2>
+          <h2 className="font-normal text-[#666666]">
+            {language
+              ? "تواصل مع فريق ليسول واختر باقتك"
+              : "Contact the Lesoll team and choose your package"}
+          </h2>
+        </div>
+        <div className="h-full sm:py-[2vh] py-[0.9vh] text-white font-semibold flex items-end">
+          <Link
+            href={"/contact-us"}
+            className="w-[195px] bg-lightGreen rounded-[6px] lg-text py-[1.7069701280227596vh] flex items-center justify-center"
+          >
+            {language ? "تواصل معنا" : "Connect with us"}
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/pricing/PriceFilter.jsx
+++ src/components/pricing/PriceFilter.jsx
@@ -3,14 +3,62 @@ import { useSelector } from "react-redux";
 
 const PriceFilter = () => {
   const language = useSelector((state) => state.GlobalState.languageIs);
-  const [timeFilter, setTimeFilter] = useState("");
+  const [timeFilter, setTimeFilter] = useState("yearly");
   return (
-    <div className="w-full flex justify-center ">
-      <div className="flex gap-x-2">
-        <button>{language ? "سنوية" : "Yearly"}</button>
-        <button>{language ? "شهرية" : "Monthly"}</button>
-        <button>{language ? "يومية" : "Daily"}</button>
-        <button>{language ? "تحت إدارة ليسول" : "Managed by Lesoll"}</button>
+    <div className="w-full flex justify-center my-[7vh]">
+      <div className="flex">
+        <div
+          className={`${timeFilter == "yearly" ? "text-lightGreen" : "text-[#CCCCCC]"} items-center flex-col flex gap-y-[2.2vh] `}
+        >
+          <button
+            onClick={() => setTimeFilter("yearly")}
+            className="px-[1vw] lg-text"
+          >
+            {language ? "سنوية" : "Yearly"}
+          </button>
+          <div
+            className={`w-full h-[1px] ${timeFilter == "yearly" ? "bg-lightGreen" : "bg-[#CCCCCC]"} `}
+          />
+        </div>
+        <div
+          className={`${timeFilter == "monthly" ? "text-lightGreen" : "text-[#CCCCCC]"} items-center flex-col flex gap-y-[2.2vh] `}
+        >
+          <button
+            onClick={() => setTimeFilter("monthly")}
+            className="px-[1vw] lg-text"
+          >
+            {language ? "شهرية" : "Monthly"}
+          </button>
+          <div
+            className={`w-full h-[1px] ${timeFilter == "monthly" ? "bg-lightGreen" : "bg-[#CCCCCC]"} `}
+          />
+        </div>
+        <div
+          className={`${timeFilter == "daily" ? "text-lightGreen" : "text-[#CCCCCC]"} items-center flex-col flex gap-y-[2.2vh] `}
+        >
+          <button
+            onClick={() => setTimeFilter("daily")}
+            className="px-[1vw] lg-text"
+          >
+            {language ? "يومية" : "Daily"}
+          </button>
+          <div
+            className={`w-full h-[1px] ${timeFilter == "daily" ? "bg-lightGreen" : "bg-[#CCCCCC]"} `}
+          />
+        </div>
+        <div
+          className={`${timeFilter == "by-lesoll" ? "text-lightGreen" : "text-[#CCCCCC]"} items-center flex-col flex gap-y-[2.2vh] `}
+        >
+          <button
+            onClick={() => setTimeFilter("by-lesoll")}
+            className="px-[1vw] lg-text"
+          >
+            {language ? "تحت إدارة ليسول" : "Managed by Lesoll"}
+          </button>
+          <div
+            className={`w-full h-[1px] ${timeFilter == "by-lesoll" ? "bg-lightGreen" : "bg-[#CCCCCC]"} `}
+          />
+        </div>
       </div>
     </div>
   );
