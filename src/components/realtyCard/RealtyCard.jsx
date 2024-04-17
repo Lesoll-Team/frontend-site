9ceec0d5484aa30e626becb5ec7ab2ce2cb0cbb2
AG-Jimmy <ahmedgamalzenhom@gmail.com>
2024-04-17 19:15:24 +0300

    Fix: first try fix LCP in category page

--- src/components/realtyCard/RealtyCard.jsx
+++ src/components/realtyCard/RealtyCard.jsx
@@ -1,5 +1,4 @@
 import { AddToFavorites } from "@/utils/propertyAPI";
-// import { Image } from "@nextui-org/react";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
@@ -11,9 +10,9 @@ import LocationAndRooms from "./basic-body-card/LocationAndRooms";
 import { getUserData } from "@/redux-store/features/auth/userProfileSlice";
 
 const RealtyCard = ({ propertyDetails }) => {
-  const userInfo = useSelector((state) => state.userProfile.userData);
+  const isMobile = typeof window !== "undefined" && window.innerWidth <= 768;
 
-  // const language = useSelector((state) => state.GlobalState.languageIs);
+  const userInfo = useSelector((state) => state.userProfile.userData);
   const dispatch = useDispatch();
 
   const [loved, setLoved] = useState(false);
@@ -32,18 +31,11 @@ const RealtyCard = ({ propertyDetails }) => {
     }
   }, [userInfo?.favorites]);
   return (
-    <div //md:w-[380px] 2xl:w-[400px]  w-full
-      className=" md:max-w-[400px]
-      md:h-[355px] h-[145px] 
-     flex   md:block overflow-hidden rounded-md bg-white drop-shadow-md  relative"
-    >
+    <div className="md:max-w-[400px] md:h-[355px] h-[145px] flex md:block overflow-hidden rounded-md bg-white drop-shadow-md  relative">
       {/* start icon favorite */}
       <div className="flex absolute md:mt-[16px] m-[10px] md:mr-[20px]">
         {userInfo && (
-          <div
-            className=" bg-white  drop-shadow-md flex justify-center w-[25px] h-[25px] md:w-[40px] md:h-[40px] items-center 
-            md:text-2xl text-md rounded-full  text-center  cursor-pointer  "
-          >
+          <div className=" bg-white  drop-shadow-md flex justify-center w-[25px] h-[25px] md:w-[40px] md:h-[40px] items-center md:text-2xl text-md rounded-full  text-center  cursor-pointer">
             {userInfo &&
               (loved ? (
                 <AiFillHeart
@@ -65,36 +57,34 @@ const RealtyCard = ({ propertyDetails }) => {
           </div>
         )}
       </div>
-
       {/* start Image */}
       <Link
         title={`${propertyDetails?.title}`}
         key={propertyDetails?._id}
         href={`/property-details/${propertyDetails?.slug}`}
-        className="
-         md:h-[174px] h-[145px] md:min-w-[480px] md:max-w-[480px]  min-w-[135px] max-w-[135px] flex"
+        className=" md:h-[174px] h-[145px] md:min-w-[480px] md:max-w-[480px]  min-w-[135px] max-w-[135px] flex"
       >
         <Image
           alt={` image  ${propertyDetails?.title}`}
           radius="none"
-          className=" flex object-cover  
-          "
-          priority={true}
-          // loading="lazy"
-          width={400}
-          height={174}
-          src={propertyDetails?.thumbnail || propertyDetails?.album[0]?.image}
+          className="flex object-cover"
+          loading="lazy"
+          width={isMobile ? 135 : 400}
+          height={isMobile ? 145 : 174}
+          placeholder="blur"
+          blurDataURL="data:image/webp;base64,UklGRpwCAABXRUJQVlA4WAoAAAAgAAAAgQAASAAASUNDUMgBAAAAAAHIAAAAAAQwAABtbnRyUkdCIFhZWiAH4AABAAEAAAAAAABhY3NwAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAQAA9tYAAQAAAADTLQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAlkZXNjAAAA8AAAACRyWFlaAAABFAAAABRnWFlaAAABKAAAABRiWFlaAAABPAAAABR3dHB0AAABUAAAABRyVFJDAAABZAAAAChnVFJDAAABZAAAAChiVFJDAAABZAAAAChjcHJ0AAABjAAAADxtbHVjAAAAAAAAAAEAAAAMZW5VUwAAAAgAAAAcAHMAUgBHAEJYWVogAAAAAAAAb6IAADj1AAADkFhZWiAAAAAAAABimQAAt4UAABjaWFlaIAAAAAAAACSgAAAPhAAAts9YWVogAAAAAAAA9tYAAQAAAADTLXBhcmEAAAAAAAQAAAACZmYAAPKnAAANWQAAE9AAAApbAAAAAAAAAABtbHVjAAAAAAAAAAEAAAAMZW5VUwAAACAAAAAcAEcAbwBvAGcAbABlACAASQBuAGMALgAgADIAMAAxADZWUDggrgAAADAIAJ0BKoIASQA/OYy6Vj6ppaSu9JtT0CcJZwaIBSMK+wc3w9wolt0xI9Ih0LbkiT5jFlmthhD+F8hxABGWhxQgTBu00Rr2dekAAP7Buijbt3iTb3XRM+9ZH9VKf8BqKtCj/mCiaQXAzSZ67W/VLmHoF+JP2oYlAqrBmBbdrd0HIPeIbXwNBsJCv1zBilNhIfBfl2b4M3EoHpYin5k3gbvrzvFbLfhkozLlghBgAA=="
+          quality={70}
+          // width={400}
+          // height={174}
+          src={propertyDetails?.thumbnail}
         />
       </Link>
-
       {/* start card data */}
       <div className=" md:p-[20px]   p-3 w-full h-full flex flex-col md:gap-y-[16px] gap-y-[6px]">
         {/* start contact and price */}
         <PriceAndSocial propertyDetails={propertyDetails} />
-
         {/* start title */}
         <TitleCard propertyDetails={propertyDetails} />
-
         {/* start location and details rooms ...etc*/}
         <LocationAndRooms propertyDetails={propertyDetails} />
       </div>
