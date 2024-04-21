681151fdf2fdef24076fdbb9f7f996ad59487ef6
Abdelrahmanmostafa0 <abdelrahmanmostafa.developer@gmail.com>
2024-04-21 15:26:55 +0200

    checking if git works

--- src/components/auth/forgetpasseord/ForgetPassword.jsx
+++ src/components/auth/forgetpasseord/ForgetPassword.jsx
@@ -1,18 +1,19 @@
 import ForgetPasswrodForm from "./ForgetPasswrodForm";
 
 const ForgetPassword = () => {
-  return (
-    <main className="grid lg:grid-cols-2 min-h-[90dvh] lg:min-h-[94dvh]">
-      {/* <div className="hidden md:flex h-100 min-h-[90dvh] bg-lightGreen items-center w-1/2 justify-start flex-col relative"> */}
-      <div className="flex h-full flex-col items-center justify-center">
-        <ForgetPasswrodForm />
-      </div>
-      <img
-        src={"/signin.png"}
-        className="object-cover lg:block hidden  h-[94dvh] w-full "
-      />
-    </main>
-  );
+   // comment
+   return (
+      <main className="grid lg:grid-cols-2 min-h-[90dvh] lg:min-h-[94dvh]">
+         {/* <div className="hidden md:flex h-100 min-h-[90dvh] bg-lightGreen items-center w-1/2 justify-start flex-col relative"> */}
+         <div className="flex h-full flex-col items-center justify-center">
+            <ForgetPasswrodForm />
+         </div>
+         <img
+            src={"/signin.png"}
+            className="object-cover lg:block hidden  h-[94dvh] w-full "
+         />
+      </main>
+   );
 };
 
 export default ForgetPassword;
