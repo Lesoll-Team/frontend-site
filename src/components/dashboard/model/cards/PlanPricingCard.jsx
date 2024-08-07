7acb8fb3551eb161a78ce73d6c572c89983ce747
AG-Jimmy <ahmedgamalzenhom@gmail.com>
2024-08-07 14:34:37 +0300

    Feature: edit payment card style

--- src/components/dashboard/model/cards/PlanPricingCard.jsx
+++ src/components/dashboard/model/cards/PlanPricingCard.jsx
@@ -10,7 +10,7 @@ const PlanPricingCard = ({ stylesCss, data, dash, planId }) => {
   return (
     <div
       key={planId}
-      className={`bg-white drop-shadow overflow-hidden flex flex-col gap-y-[16px] w-full  rounded-[6px] relative ${data?.Popular ? "h-[600px]" : "h-[550px]"}`}
+      className={`bg-white drop-shadow overflow-hidden flex flex-col gap-y-[16px] w-full  rounded-[6px] relative h-[550px]`}
     >
       <div dir={language ? "ltr" : "rtl"} className={`flex w-full relative`}>
         {data?.Popular && <PopularTag />}
